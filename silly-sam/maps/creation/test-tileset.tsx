f22f2919b694d0093117f5c12a41b703b0096f97
davidjtferguson <davidjtferguson@gmail.com>
2018-09-02 09:16:21 +0100

    added some test map files to play around with

---
+++ silly-sam/maps/creation/test-tileset.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test-tileset" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="../../assets/art/map/test-tileset-image.png" width="32" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
