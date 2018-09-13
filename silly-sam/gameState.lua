26216a80d267b59f8f72a0ac08d9c8c5982cab6b
davidjtferguson <davidjtferguson@gmail.com>
2018-09-13 20:42:25 +0100

    Converted GameState to hump class

--- silly-sam/gameState.lua
+++ silly-sam/gameState.lua
@@ -1,48 +1,47 @@
-GameState = {}
-GameState.__index = GameState
+local Class = require "hump.class"
 
-function GameState:create()
-    local gameState = {}
-    setmetatable(gameState, GameState)
+GameState = Class{}
+
+function GameState:init()
 
     -- create a physics world
     -- maybe the physics manager should own the world?
     love.physics.setMeter(100)
-    gameState.physicsWorld = love.physics.newWorld(0, 10*100, true)
+    self.physicsWorld = love.physics.newWorld(0, 10*100, true)
 
     -- load the map
     local Sti = require "Simple-Tiled-Implementation/sti"
-    gameState.map = Sti("maps/test-map-limited-level.lua", { "box2d" })
+    self.map = Sti("maps/test-map-limited-level.lua", { "box2d" })
 
-    gameState.map:box2d_init(gameState.physicsWorld)
+    self.map:box2d_init(self.physicsWorld)
 
-    gameState.physicsWorld:setCallbacks(
+    self.physicsWorld:setCallbacks(
         function(body1, body2, contact)
-            gameState:beginContact(body1, body2, contact)
+            self:beginContact(body1, body2, contact)
         end,
         function(body1, body2, contact)
-            gameState:endContact(body1, body2, contact)
+            self:endContact(body1, body2, contact)
         end,
         function(body1, body2, contact)
-            gameState:preSolve(body1, body2, contact)
+            self:preSolve(body1, body2, contact)
         end,
         function(body1, body2, contact)
-            gameState:postSolve(body1, body2, contact)
+            self:postSolve(body1, body2, contact)
         end
     )
 
     -- create sam instance
     local Sam = require "sam"
-    gameState.sam = Sam(gameState.physicsWorld)
+    self.sam = Sam(self.physicsWorld)
 
     -- make camera focus on Sam
     local Camera = require "hump.camera"
-    gameState.camera = Camera(gameState.sam.chest.body:getPosition())
+    self.camera = Camera(self.sam.chest.body:getPosition())
 
-    gameState.controls = {
+    self.controls = {
         bindings = {
-            left = function() gameState.sam:moveLeft() end,
-            right = function() gameState.sam:moveRight() end,
+            left = function() self.sam:moveLeft() end,
+            right = function() self.sam:moveRight() end,
             start = reset,
         },
         keysPressed = {
@@ -56,8 +55,6 @@ function GameState:create()
             start = "start",
         }
     }
-
-    return gameState
 end
 
 function GameState:update(dt)
--- silly-sam/sam.lua
+++ silly-sam/sam.lua
@@ -1,4 +1,4 @@
-Class = require "hump.class"
+local Class = require "hump.class"
 
 Sam = Class{}
 
