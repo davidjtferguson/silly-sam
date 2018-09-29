948dfd4c700536153176be595c130ee843e52a27
davidjtferguson <davidjtferguson@gmail.com>
2018-09-29 17:50:14 +0100

    improved hanging bag visuals

--- silly-sam/toys/hangingBag.lua
+++ silly-sam/toys/hangingBag.lua
@@ -7,15 +7,15 @@ function HangingBag:init(world, xSpawn, ySpawn, ropeLength, bagWidth, bagHeight,
     self.bagWidth = bagWidth
     self.bagHeight = bagHeight
 
+    self.ropeColour = {0.9, 0.9, 0.9}
+
     -- create a static anchor point
     self.anchor = {}
     self.anchor.body = love.physics.newBody(world, xSpawn, ySpawn, "static")
     self.anchor.shape = love.physics.newCircleShape(5)
     self.anchor.fixture = love.physics.newFixture(self.anchor.body, self.anchor.shape, 0.5);
     self.anchor.fixture:setFriction(0.5)
-    self.anchor.color = {0.80, 0.20, 0.20}
-
-    self.ropeColour = {0.9, 0.9, 0.9}
+    self.anchor.color = {0.5, 0.5, 0.5}
 
     self.bag = {}
     self.bag.body = love.physics.newBody(world, xSpawn, ySpawn+ropeLength, "dynamic")
@@ -26,13 +26,13 @@ function HangingBag:init(world, xSpawn, ySpawn, ropeLength, bagWidth, bagHeight,
     self.bag.color = {0.2, 0.2, 0.2}
 
     if pivotingJoint then
-        -- create another object between the bag and the rope to allow the bag to rotate around it
+        -- create another object between the bag and the rope to allow the bag to rotate around the join to the rope
         self.bagPivotPoint = {}
         self.bagPivotPoint.body = love.physics.newBody(world, xSpawn, ySpawn+ropeLength-bagHeight/2, "dynamic")
         self.bagPivotPoint.shape = love.physics.newCircleShape(5)
         self.bagPivotPoint.fixture = love.physics.newFixture(self.bagPivotPoint.body, self.bagPivotPoint.shape, 0.5);
         self.bagPivotPoint.fixture:setFriction(0.5)
-        self.bagPivotPoint.color = {0.20, 0.80, 0.20}
+        self.bagPivotPoint.color = {0.5, 0.5, 0.5}
 
         -- join pivot to the anchor
         self.anchor.joint = love.physics.newRevoluteJoint(self.anchor.body, self.bagPivotPoint.body, xSpawn, ySpawn)
@@ -40,26 +40,25 @@ function HangingBag:init(world, xSpawn, ySpawn, ropeLength, bagWidth, bagHeight,
         -- join bag to pivot
         self.bag.joint = love.physics.newRevoluteJoint(self.bag.body, self.bagPivotPoint.body, self.bagPivotPoint.body:getX(), self.bagPivotPoint.body:getY())
     else
-        -- connect the anchor to the bag
+        -- connect the anchor to the bag directly
         self.anchor.joint = love.physics.newRevoluteJoint(self.anchor.body, self.bag.body, xSpawn, ySpawn)
     end
 end
 
 function HangingBag:draw()
+    -- draw rope
     love.graphics.setLineWidth(10)
-
-
+    love.graphics.setColor(self.ropeColour)
     if self.bagPivotPoint then
-        love.graphics.setColor(self.ropeColour)
         love.graphics.line(self.anchor.body:getX(), self.anchor.body:getY(), self.bagPivotPoint.body:getX(), self.bagPivotPoint.body:getY())
+
+        -- some indicator of what type of bag it is
         self:drawCirclePhysicsObject(self.bagPivotPoint)
     else
-        love.graphics.setColor(self.ropeColour)
-        love.graphics.line(self.anchor.body:getX(), self.anchor.body:getY(), self.bag.body:getX(), self.bag.body:getY()-self.bagHeight/2)
+        love.graphics.line(self.anchor.body:getX(), self.anchor.body:getY(), self.bag.body:getX(), self.bag.body:getY())
     end
     
     self:drawCirclePhysicsObject(self.anchor)
-
     self:drawRectPhysicsObject(self.bag)
 end
 
