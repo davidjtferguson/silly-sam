94385f494eaef60eb1158c44f5adff56847c9f02
rcsweeney <rcsweeney@live.co.uk>
2019-01-12 19:18:40 +0100

    level work - tryin to get shit happenin

--- map-editing/environment-test-tileset.tsx
+++ map-editing/environment-test-tileset.tsx
@@ -1,20 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="test" tilewidth="2092" tileheight="1517" tilecount="13" columns="0">
+<tileset name="test" tilewidth="2092" tileheight="1221" tilecount="19" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="763" height="1517" source="../silly-sam/assets/art/environment-textures/tree-triangle.png"/>
+  <image width="390" height="775" source="../silly-sam/assets/art/environment-textures/tree-triangle.png"/>
  </tile>
  <tile id="1">
-  <image width="698" height="1347" source="../silly-sam/assets/art/environment-textures/tree-square.png"/>
+  <image width="353" height="680" source="../silly-sam/assets/art/environment-textures/tree-square.png"/>
  </tile>
  <tile id="2">
-  <image width="807" height="1457" source="../silly-sam/assets/art/environment-textures/tree-leaf.png"/>
+  <image width="332" height="600" source="../silly-sam/assets/art/environment-textures/tree-leaf.png"/>
  </tile>
  <tile id="3">
-  <image width="576" height="397" source="../silly-sam/assets/art/environment-textures/cloud-001.png"/>
+  <image width="337" height="193" source="../silly-sam/assets/art/environment-textures/cloud-001.png"/>
  </tile>
  <tile id="4">
-  <image width="557" height="262" source="../silly-sam/assets/art/environment-textures/cloud-002.png"/>
+  <image width="315" height="144" source="../silly-sam/assets/art/environment-textures/cloud-002.png"/>
  </tile>
  <tile id="5">
   <image width="1879" height="163" source="../silly-sam/assets/art/environment-textures/grass-long.png"/>
@@ -29,15 +29,33 @@
   <image width="935" height="109" source="../silly-sam/assets/art/environment-textures/grass-short-002.png"/>
  </tile>
  <tile id="9">
-  <image width="600" height="81" source="../silly-sam/assets/art/environment-textures/grass-tuft-001.png"/>
+  <image width="315" height="43" source="../silly-sam/assets/art/environment-textures/grass-tuft-001.png"/>
  </tile>
  <tile id="10">
-  <image width="539" height="87" source="../silly-sam/assets/art/environment-textures/grass-tuft-002.png"/>
+  <image width="331" height="52" source="../silly-sam/assets/art/environment-textures/grass-tuft-002.png"/>
  </tile>
  <tile id="11">
-  <image width="478" height="97" source="../silly-sam/assets/art/environment-textures/grass-tuft-003.png"/>
+  <image width="284" height="58" source="../silly-sam/assets/art/environment-textures/grass-tuft-003.png"/>
  </tile>
  <tile id="12">
   <image width="769" height="1221" source="../silly-sam/assets/art/environment-textures/groupedtrees-001.png"/>
+ </tile>
+ <tile id="13">
+  <image width="353" height="48" source="../silly-sam/assets/art/environment-textures/grass-tuft-004.png"/>
+ </tile>
+ <tile id="14">
+  <image width="317" height="51" source="../silly-sam/assets/art/environment-textures/grass-tuft-005.png"/>
+ </tile>
+ <tile id="15">
+  <image width="280" height="57" source="../silly-sam/assets/art/environment-textures/grass-tuft-006.png"/>
+ </tile>
+ <tile id="16">
+  <image width="177" height="179" source="../silly-sam/assets/art/environment-textures/ball.png"/>
+ </tile>
+ <tile id="17">
+  <image width="574" height="566" source="../silly-sam/assets/art/environment-textures/ground-base.png"/>
+ </tile>
+ <tile id="18">
+  <image width="426" height="426" source="../silly-sam/assets/art/environment-textures/ground-base-circle.png"/>
  </tile>
 </tileset>
