835cec28c74587be71bbd111052f17b4a8c87020
rcsweeney <rcsweeney@live.co.uk>
2019-01-12 17:56:16 +0100

    level work

---
+++ map-editing/environment-test-tileset.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test" tilewidth="2092" tileheight="1517" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="763" height="1517" source="../silly-sam/assets/art/environment-textures/tree-triangle.png"/>
+ </tile>
+ <tile id="1">
+  <image width="698" height="1347" source="../silly-sam/assets/art/environment-textures/tree-square.png"/>
+ </tile>
+ <tile id="2">
+  <image width="807" height="1457" source="../silly-sam/assets/art/environment-textures/tree-leaf.png"/>
+ </tile>
+ <tile id="3">
+  <image width="576" height="397" source="../silly-sam/assets/art/environment-textures/cloud-001.png"/>
+ </tile>
+ <tile id="4">
+  <image width="557" height="262" source="../silly-sam/assets/art/environment-textures/cloud-002.png"/>
+ </tile>
+ <tile id="5">
+  <image width="1879" height="163" source="../silly-sam/assets/art/environment-textures/grass-long.png"/>
+ </tile>
+ <tile id="6">
+  <image width="2092" height="87" source="../silly-sam/assets/art/environment-textures/grass-overhang.png"/>
+ </tile>
+ <tile id="7">
+  <image width="897" height="106" source="../silly-sam/assets/art/environment-textures/grass-short-001.png"/>
+ </tile>
+ <tile id="8">
+  <image width="935" height="109" source="../silly-sam/assets/art/environment-textures/grass-short-002.png"/>
+ </tile>
+ <tile id="9">
+  <image width="600" height="81" source="../silly-sam/assets/art/environment-textures/grass-tuft-001.png"/>
+ </tile>
+ <tile id="10">
+  <image width="539" height="87" source="../silly-sam/assets/art/environment-textures/grass-tuft-002.png"/>
+ </tile>
+ <tile id="11">
+  <image width="478" height="97" source="../silly-sam/assets/art/environment-textures/grass-tuft-003.png"/>
+ </tile>
+ <tile id="12">
+  <image width="769" height="1221" source="../silly-sam/assets/art/environment-textures/groupedtrees-001.png"/>
+ </tile>
+</tileset>
