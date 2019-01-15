6500798af24503145f628c713d3f14aa169c2165
rcsweeney <rcsweeney@live.co.uk>
2019-01-15 20:24:05 +0100

    furtherlevel work

---
+++ map-editing/grass-tuft-006-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass-tuft-006" tilewidth="280" tileheight="57" tilecount="1" columns="1">
+ <image source="../silly-sam/assets/art/environment-textures/grass-tuft-006.png" width="280" height="57"/>
+</tileset>
---
+++ map-editing/tree-leaf-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tree-leaf" tilewidth="332" tileheight="600" tilecount="1" columns="1">
+ <image source="../silly-sam/assets/art/environment-textures/tree-leaf.png" width="332" height="600"/>
+</tileset>
