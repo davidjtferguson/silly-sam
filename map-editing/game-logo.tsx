5ccab009af506e0976bc0babc113a882ae495c70
rcsweeney <rcsweeney@live.co.uk>
2019-01-20 16:45:12 +0100

    logo, logo level, icons and new textures

---
+++ map-editing/game-logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="game-logo" tilewidth="832" tileheight="313" tilecount="1" columns="1">
+ <image source="../silly-sam/assets/art/game-logo.png" width="832" height="313"/>
+</tileset>
