601c1e848ac0a3622c828c72dfddf6422f19caa2
rcsweeney <rcsweeney@live.co.uk>
2019-02-28 20:45:04 +0100

    starting map art pass

---
+++ map-editing/tilesets/sky.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sky" tilewidth="375" tileheight="375" tilecount="1" columns="1">
+ <image source="../../silly-sam/assets/art/map/sky.png" width="396" height="376"/>
+</tileset>
