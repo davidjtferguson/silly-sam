d5baae270f9db6aac790a0e5636c9b074bcef07b
rcsweeney <rcsweeney@live.co.uk>
2019-04-07 15:54:16 +0200

    art pass bonus and rory level tweaks

--- silly-sam/states/gameState.lua
+++ silly-sam/states/gameState.lua
@@ -25,12 +25,12 @@ local function checkStaticBool(static)
 end
 
 function GameState:init()
-    --self:loadMap("maps/intro-map.lua")
+    self:loadMap("maps/intro-map.lua")
     --self:loadMap("maps/survival-map.lua")
     --self:loadMap("maps/cliff.lua")
     --self:loadMap("maps/swinging.lua")
     --self:loadMap("maps/bonus.lua")
-    self:loadMap("maps/rory-level.lua")
+    --self:loadMap("maps/rory-level.lua")
     
     --self:loadMap("maps/test-map-limited-level.lua")
 
